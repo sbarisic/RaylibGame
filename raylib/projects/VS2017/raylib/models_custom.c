52496c064628b0f828d30af0b9603590c60ae3c6
Saša Barišić <cartmanium@gmail.com>
2025-06-05 15:54:35 +0200

    Project files 1

--- raylib/projects/VS2017/raylib/models_custom.c
+++ raylib/projects/VS2017/raylib/models_custom.c
@@ -16,23 +16,23 @@ extern "C" {            // Prevents name mangling of functions
 
 	RLAPI Mesh GenMeshRaw(void* vertices, int vertices_len, void* indices, int indices_len, void* texcoords, int texcoords_len, void* normals, int normals_len, void* colors, int colors_len) {
 		Mesh mesh = { 0 };
-		mesh.vboId = (unsigned int *)RL_CALLOC(7 * sizeof(unsigned int), 1);
+		mesh.vboId = (unsigned int*)RL_CALLOC(7 * sizeof(unsigned int), 1);
 
-		mesh.vertices = (float *)RL_MALLOC(vertices_len);
+		mesh.vertices = (float*)RL_MALLOC(vertices_len);
 		memcpy(mesh.vertices, vertices, vertices_len);
 
 		if (texcoords != NULL) {
-			mesh.texcoords = (float *)RL_MALLOC(texcoords_len);
+			mesh.texcoords = (float*)RL_MALLOC(texcoords_len);
 			memcpy(mesh.texcoords, texcoords, texcoords_len);
 		}
 
 		if (normals != NULL) {
-			mesh.normals = (float *)RL_MALLOC(normals_len);
+			mesh.normals = (float*)RL_MALLOC(normals_len);
 			memcpy(mesh.normals, normals, normals_len);
 		}
 
 		if (indices != NULL) {
-			mesh.indices = (unsigned short *)RL_MALLOC(indices_len);
+			mesh.indices = (unsigned short*)RL_MALLOC(indices_len);
 			memcpy(mesh.indices, indices, indices_len);
 		}
 
@@ -44,7 +44,7 @@ extern "C" {            // Prevents name mangling of functions
 		mesh.vertexCount = vertices_len / (sizeof(float) * 3);
 		mesh.triangleCount = mesh.vertexCount / 3;
 
-		rlLoadMesh(&mesh, false);
+		UploadMesh(&mesh, false);
 		return mesh;
 	}
 

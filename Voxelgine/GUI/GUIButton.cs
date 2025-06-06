9fa7d2464679217590b9546a66f359731aab120e
Saša Barišić <cartmanium@gmail.com>
2025-06-06 20:54:07 +0200

    Heart icons Refactoring GUI elements into new files

---
+++ Voxelgine/GUI/GUIButton.cs
@@ -0,0 +1,121 @@
+﻿using Raylib_cs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using Voxelgine.Engine;
+
+namespace Voxelgine.GUI {
+	class GUIButton : GUIElement {
+		Texture2D BtnTex;
+		Texture2D BtnHoverTex;
+		Texture2D BtnPressTex;
+
+		Texture2D Icon;
+		float IconScale = 3;
+		bool HasIcon = false;
+
+		GUIManager Mgr;
+
+		public string Text;
+		public OnMouseClickedFunc OnClickedFunc;
+
+		public GUIButton(GUIManager Mgr) {
+			this.Mgr = Mgr;
+
+			BtnTex = ResMgr.GetTexture("gui/btn.png");
+			Raylib.SetTextureFilter(BtnTex, TextureFilter.Point);
+
+			BtnHoverTex = ResMgr.GetTexture("gui/btn_hover.png");
+			Raylib.SetTextureFilter(BtnHoverTex, TextureFilter.Point);
+
+			BtnPressTex = ResMgr.GetTexture("gui/btn_press.png");
+			Raylib.SetTextureFilter(BtnPressTex, TextureFilter.Point);
+		}
+
+		public void SetIcon(Texture2D? Icon) {
+			if (Icon.HasValue) {
+				HasIcon = true;
+				this.Icon = Icon.Value;
+			} else {
+				HasIcon = false;
+			}
+		}
+
+		public virtual void OnMouseClick() {
+			OnClickedFunc?.Invoke(this);
+		}
+
+		bool ButtonHeldDown = false;
+
+		//Stopwatch SWatc = Stopwatch.StartNew();
+
+		public override void Update(float Dt) {
+			//IconScale = 1 + ((MathF.Sin(SWatc.ElapsedMilliseconds / 1000.0f) + 1) / 2) * 3;
+
+			if (IsInside(MousePos)) {
+				if (Raylib.IsMouseButtonDown(MouseButton.Left)) {
+					if (Raylib.IsMouseButtonPressed(MouseButton.Left) && !ButtonHeldDown) {
+						ButtonHeldDown = true;
+					} else {
+					}
+				} else if (Raylib.IsMouseButtonReleased(MouseButton.Left)) {
+					if (ButtonHeldDown) {
+						ButtonHeldDown = false;
+						OnMouseClick();
+					}
+				} else {
+					ButtonHeldDown = false;
+				}
+			} else {
+				if (!Raylib.IsMouseButtonDown(MouseButton.Left)) {
+					ButtonHeldDown = false;
+				}
+			}
+		}
+
+		public override void Draw(bool Hovered, bool MouseClicked, bool MouseDown) {
+			Rectangle BtnLoc = new Rectangle(Pos, Size);
+			Texture2D Tex = BtnTex;
+			Vector2 DrawOffset = Vector2.Zero;
+
+			if (Hovered) {
+				if (MouseDown) {
+					Tex = BtnPressTex;
+					DrawOffset = new Vector2(4, 4);
+				} else
+					Tex = BtnHoverTex;
+			}
+
+			Mgr.Draw9Patch(Tex, BtnLoc, Color.White);
+
+			Vector2 IconSize = Vector2.Zero;
+			Vector2 IconOffset = Vector2.Zero;
+			if (HasIcon) {
+				IconSize = new Vector2(Icon.Width, Icon.Height) * IconScale;
+				IconOffset = new Vector2(IconSize.X / 2 + 10, 0);
+			}
+
+			Vector2 TxtSize = Mgr.MeasureText(Text) - IconOffset;
+			Vector2 TxtPos = Pos + Size / 2 - TxtSize / 2;
+
+			Mgr.DrawText(Text, TxtPos + DrawOffset, Color.White);
+
+			if (HasIcon) {
+				// Vector2 IconPos = Pos + (Size / 2) - (TxtSize / 2) + DrawOffset - (IconSize / 2);
+				Vector2 IconPos = TxtPos + new Vector2(0, TxtSize.Y / 2) - IconOffset;
+
+				//float TT = SWatc.ElapsedMilliseconds / 1000.0f * 50;
+				float TT = 0;
+
+				//Mgr.DrawRectLines(IconPos + DrawOffset, IconSize, Color.Blue);
+				Mgr.DrawTexture(Icon, IconPos + DrawOffset, TT, IconScale);
+			}
+		}
+	}
+
+}
---
+++ Voxelgine/GUI/GUIItemBox.cs
@@ -0,0 +1,95 @@
+﻿using Raylib_cs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using Voxelgine.Engine;
+
+namespace Voxelgine.GUI {
+	class GUIItemBox : GUIElement {
+		public bool IsSelected = false;
+
+		public string Text;
+
+		Texture2D Tex;
+		Texture2D TexSel;
+
+		Vector2 IconSize;
+		Texture2D Icon;
+		bool HasIcon = false;
+		float IconScale = 2.0f;
+
+		GUIManager Mgr;
+
+		public OnMouseClickedFunc OnClickedFunc;
+
+		public GUIItemBox(GUIManager Mgr) {
+			this.Mgr = Mgr;
+
+			Tex = ResMgr.GetTexture("gui/itembox.png");
+			TexSel = ResMgr.GetTexture("gui/itembox_sel.png");
+			Raylib.SetTextureFilter(Tex, TextureFilter.Point);
+		}
+
+		public void SetIcon(Texture2D? Icon, float Scale) {
+			if (Icon.HasValue) {
+				HasIcon = true;
+				this.Icon = Icon.Value;
+				this.IconScale = Scale;
+				IconSize = new Vector2(this.Icon.Width, this.Icon.Height);
+			} else {
+				HasIcon = false;
+			}
+		}
+
+		public virtual void OnMouseClick() {
+			OnClickedFunc?.Invoke(this);
+		}
+
+		bool ButtonHeldDown = false;
+
+		public override void Update(float Dt) {
+			if (IsInside(MousePos)) {
+				if (Raylib.IsMouseButtonDown(MouseButton.Left)) {
+					if (Raylib.IsMouseButtonPressed(MouseButton.Left) && !ButtonHeldDown) {
+						ButtonHeldDown = true;
+					} else {
+					}
+				} else if (Raylib.IsMouseButtonReleased(MouseButton.Left)) {
+					if (ButtonHeldDown) {
+						ButtonHeldDown = false;
+						OnMouseClick();
+					}
+				} else {
+					ButtonHeldDown = false;
+				}
+			} else {
+				if (!Raylib.IsMouseButtonDown(MouseButton.Left)) {
+					ButtonHeldDown = false;
+				}
+			}
+		}
+
+		public override void Draw(bool Hovered, bool MouseClicked, bool MouseDown) {
+			Rectangle BtnLoc = new Rectangle(Pos, Size);
+
+			if (IsSelected) {
+				Mgr.Draw9Patch(TexSel, BtnLoc, Color.White);
+			} else {
+				Mgr.Draw9Patch(Tex, BtnLoc, Color.White);
+			}
+
+			float Clr = ButtonHeldDown ? 0.8f : 1.0f;
+
+			Mgr.DrawTexture(Icon, Pos + Size / 2, 0, IconScale, new Color(Clr, Clr, Clr));
+
+			if (!string.IsNullOrEmpty(Text)) {
+				Mgr.DrawTextOutline(Text, Pos + Size / 2, Color.White, 2);
+			}
+		}
+	}
+}

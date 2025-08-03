88d14ebe3759767c903c702ba71a24bab3c1da23
Saša Barišić <cartmanium@gmail.com>
2025-08-04 00:18:09 +0200

    New easings

--- Voxelgine/Engine/Animations/LerpManager.cs
+++ Voxelgine/Engine/Animations/LerpManager.cs
@@ -41,5 +41,31 @@ namespace Voxelgine.Engine {
 		public static float EaseInOutQuart(float T) {
 			return T < 0.5 ? 8 * T * T * T * T : 1 - MathF.Pow(-2 * T + 2, 4) / 2;
 		}
+
+		public static float EaseInBounce(float x) {
+			return 1 - EaseOutBounce(1 - x);
+
+		}
+
+		public static float EaseOutBounce(float x) {
+			const float n1 = 7.5625f;
+			const float d1 = 2.75f;
+
+			if (x < 1 / d1) {
+				return n1 * x * x;
+			} else if (x < 2 / d1) {
+				return n1 * (x -= 1.5f / d1) * x + 0.75f;
+			} else if (x < 2.5 / d1) {
+				return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+			} else {
+				return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+			}
+		}
+
+		public static float EaseInOutBounce(float x) {
+			return x < 0.5
+			  ? (1 - EaseOutBounce(1 - 2 * x)) / 2
+			  : (1 + EaseOutBounce(2 * x - 1)) / 2;
+		}
 	}
 }

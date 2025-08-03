e24bb67b9a91ee1bcd5d43a517ac1c6426956c89
Saša Barišić <cartmanium@gmail.com>
2025-08-03 20:31:19 +0200

    ScissorManager implementation

--- Voxelgine/Graphics/ScissorManager.cs
+++ Voxelgine/Graphics/ScissorManager.cs
@@ -15,35 +15,47 @@ namespace Voxelgine.Graphics {
 
 		// TODO: Implement the rest of the functionality to clip rectangles
 		static Rectangle ClipAllRects() {
-			Rectangle Result = new Rectangle();
-			Rectangle[] Rects = ScissorStack.ToArray();
+			if (ScissorStack.Count == 0)
+				return new Rectangle(0, 0, 0, 0);
 
-			for (int i = 0; i < Rects.Length; i++) {
-				// TODO: Calculate inner most rectangle into result
+			// Stack is LIFO, but we want to clip from bottom to top (outer to inner)
+			Rectangle[] rects = ScissorStack.Reverse().ToArray();
+			Rectangle result = rects[0];
+			for (int i = 1; i < rects.Length; i++) {
+				Rectangle r = rects[i];
+				float x1 = MathF.Max(result.X, r.X);
+				float y1 = MathF.Max(result.Y, r.Y);
+				float x2 = MathF.Min(result.X + result.Width, r.X + r.Width);
+				float y2 = MathF.Min(result.Y + result.Height, r.Y + r.Height);
+				float w = MathF.Max(0, x2 - x1);
+				float h = MathF.Max(0, y2 - y1);
+				result = new Rectangle(x1, y1, w, h);
 			}
-
-			return Result;
+			return result;
 		}
 
 		public static void BeginScissor(float X, float Y, float W, float H) {
-			Rectangle ScissorRect = new Rectangle(X, Y, W, H);
-			ScissorStack.Push(ScissorRect);
-
-			if (ScissorStack.Count > 0) {
-				ScissorRect = ClipAllRects();
-			}
-
-			Raylib.BeginScissorMode((int)ScissorRect.X, (int)ScissorRect.Y, (int)ScissorRect.Width, (int)ScissorRect.Height);
+			Rectangle newRect = new Rectangle(X, Y, W, H);
+			ScissorStack.Push(newRect);
+			Rectangle scissorRect = ClipAllRects();
+			CurrentScissorRect = scissorRect;
+			Raylib.BeginScissorMode((int)scissorRect.X, (int)scissorRect.Y, (int)scissorRect.Width, (int)scissorRect.Height);
 			ScissorCount++;
 		}
 
 		public static void EndScissor() {
+			if (ScissorStack.Count == 0) {
+				throw new InvalidOperationException("ScissorManager: EndScissor called without matching BeginScissor");
+			}
 			ScissorStack.Pop();
 			ScissorCount--;
-
 			if (ScissorCount == 0) {
 				Raylib.EndScissorMode();
-			} else if (ScissorCount < 0) {
+			} else if (ScissorCount > 0) {
+				Rectangle scissorRect = ClipAllRects();
+				CurrentScissorRect = scissorRect;
+				Raylib.BeginScissorMode((int)scissorRect.X, (int)scissorRect.Y, (int)scissorRect.Width, (int)scissorRect.Height);
+			} else {
 				throw new InvalidOperationException("ScissorManager: EndScissor called without matching BeginScissor");
 			}
 		}

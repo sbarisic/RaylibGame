a0a6bcd30f048882ca9b016a9e5199198b605e04
Saša Barišić <cartmanium@gmail.com>
2025-08-03 19:54:42 +0200

    GUIInputBox

---
+++ Voxelgine/GUI/GUIInputBox.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voxelgine.GUI {
+	class GUIInputBox : GUIElement {
+		GUIManager Mgr;
+
+		public string Label = "DefaultLabel";
+		public string Value = "";
+
+		public GUIInputBox(GUIManager Mgr, string Label, string Value) {
+			this.Mgr = Mgr;
+			this.Label = Label;
+			this.Value = Value;
+		}
+
+		public override void Draw(bool Hovered, bool MouseClicked, bool MouseDown) {
+			throw new NotImplementedException();
+		}
+	}
+}
